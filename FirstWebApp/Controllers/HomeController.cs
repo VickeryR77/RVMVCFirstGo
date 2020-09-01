8aac167313c41419f2c37a259b7e46ba49088363
Rick Vickery <rickvickery@quickenloans.com>
2020-09-01 17:49:02 -0400

    These are the files for the MVC project

---
+++ FirstWebApp/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Web.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ThankYou(string firstname, string lastname, string email, string password) //Named the same as in HTML
+        {
+
+            Regex reg = new Regex(@"^[A-Za-z0-9]{1}[a-zA-Z0-9]{0,20}$");
+
+            if (!reg.IsMatch(password))
+            {
+                System.Threading.Thread.Sleep(1000);
+                return View("Error");
+            }
+            else
+            {
+                string hidePassword = "";
+                foreach (char c in password)
+                {
+                    hidePassword += "*";
+                }
+
+                ViewBag.Welcome = $"Welcome,  {firstname} {lastname}!";
+                ViewBag.ShowName = $"Email   : {email}";
+                ViewBag.ShowPass = $"Password: {hidePassword}";
+
+                return View();
+            }
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
